b51b33934c2372445b6e0173c39e8a08e23f6126
王亚文 <Mr.wyw@icloud.com>
2019-02-17 16:42:41 +0800

    NN & KNN

--- CS231n/CS231n.py
+++ CS231n/CS231n.py
@@ -1,1 +1,13 @@
+import numpy as np
 
+from ImageClassification.NearestNeighborclassifier import NearestNeighbor
+
+if __name__ == '__main__':
+	instance = NearestNeighbor()
+
+	X = np.arange(9).reshape(3, 3)
+	y = np.array([[1], [2], [3]])
+
+	x = np.array([[1, 2, 3], [4, 5, 6]])
+	instance.train(X, y)
+	print(instance.predict(x))
